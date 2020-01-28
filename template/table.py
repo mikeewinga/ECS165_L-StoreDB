1dea3425c6c49697a53d707504b73a59d9ea9104
volatio <kaiyoshida@ucdavis.edu>
2020-01-27 17:39:00 -0800

    Add growing base table, separate tail table

--- template/table.py
+++ template/table.py
@@ -25,25 +25,37 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
+        self.total_columns = num_columns + 4
         self.page_directory = {}
-        for x in range((self.num_columns + 4)*2):
-            self.page_directory[x] = Page()
+        for x in range((self.num_columns + 4)):
+            self.page_directory[(0,x)] = Page()
+            self.page_directory[(1,x)] = Page()
         self.current_Rid = 0
         pass
 
     def insert(self, schema, record):
-        self.page_directory[0].write(0)
-        self.page_directory[1].write(self.current_Rid)
-        self.page_directory[2].write(0)
-        self.page_directory[3].write(schema)
+        offSet = 0;
+        while not self.page_directory[(0,offSet)].has_capacity():
+            offSet = offSet + self.num_columns + 4
+        self.page_directory[(0,0+offSet)].write(0)
+        self.page_directory[(0,1+offSet)].write(self.current_Rid)
+        self.page_directory[(0,2+offSet)].write(0)
+        self.page_directory[(0,3+offSet)].write(schema)
         self.current_Rid = self.current_Rid + 1
-        print(record.columns)
+        #print(self.current_Rid)
         for x in range(self.num_columns):
-            self.page_directory[x + 4].write(record.columns[x])
+            self.page_directory[(0,x + 4+offSet)].write(record.columns[x])
+        if not self.page_directory[(0,offSet)].has_capacity():
+            for x in range(self.num_columns + 4):
+                self.page_directory[(0,x + self.total_columns)] = Page()
+            self.total_columns = self.total_columns + self.num_columns + 4
+            
             
     def debugRead(self, index):
+        offSet = (int)(index // (PAGESIZE/DATASIZE))*(4+self.num_columns)
+        newIndex = (int)(index % (PAGESIZE/DATASIZE))
         for x in range(4 + self.num_columns):
-            print(int.from_bytes(self.page_directory[x].read(index), byteorder = "big"), end =" ")
+            print(int.from_bytes(self.page_directory[(0,x+offSet)].read(newIndex), byteorder = "big"), end =" ")
         print()
 
     def __merge(self):
