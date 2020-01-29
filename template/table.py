6fa282d972d6e8e79721d7ac5a20cbda84605c4a
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-01-28 23:29:04 -0800

    Added col_type array to keep track of data types of the columns. Print funciton available with use of unpack, may need more flexability added to it later.

--- template/table.py
+++ template/table.py
@@ -1,5 +1,6 @@
 from page import *
 from time import time
+from struct import unpack_from
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -25,9 +26,50 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
+<<<<<<< Updated upstream
+=======
+        self.total_columns = num_columns + 4
+        self.col_types = ['i','i','i','s']
+>>>>>>> Stashed changes
         self.page_directory = {}
         pass
 
+<<<<<<< Updated upstream
+=======
+    def insert(self, schema, record):
+        offSet = 0;
+        while not self.page_directory[(0,offSet)].has_capacity():
+            offSet = offSet + self.num_columns + 4
+        self.current_Rid = self.current_Rid + 1
+        self.page_directory[(0,0+offSet)].write(0)
+        self.page_directory[(0,1+offSet)].write(self.current_Rid)
+        self.page_directory[(0,2+offSet)].write(0)
+        self.page_directory[(0,3+offSet)].write(schema)
+        #print(self.current_Rid)
+        for x in range(self.num_columns):
+            self.page_directory[(0,x + 4+offSet)].write(record.columns[x])
+            if(self.current_Rid==1):
+                temp_char=str(type(record.columns[x]))
+                #Returns the 8th character in the sting (i.e. data-type designator)
+                self.col_types.append(temp_char[8])
+                #The function below will print the value in the data-type of the
+                #original input. This does not work well with schema encoding.
+                #temp_tuple = unpack_from( '>i'+self.col_types[x+4] ,self.page_directory[(0,x+4)].read(0))
+                #print(temp_tuple[1])
+        if not self.page_directory[(0,offSet)].has_capacity():
+            for x in range(self.num_columns + 4):
+                self.page_directory[(0,x + self.total_columns)] = Page()
+            self.total_columns = self.total_columns + self.num_columns + 4
+            
+            
+    def debugRead(self, index):
+        offSet = (int)(index // (PAGESIZE/DATASIZE))*(4+self.num_columns)
+        newIndex = (int)(index % (PAGESIZE/DATASIZE))
+        for x in range(4 + self.num_columns):
+            print(int.from_bytes(self.page_directory[(0,x+offSet)].read(newIndex), byteorder = "big"), end =" ")
+        print()
+
+>>>>>>> Stashed changes
     def __merge(self):
         pass
  
