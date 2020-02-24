ff0cc035c6aeb48bebe113948222ac98c6096262
Amber Lee <amblee2009@gmail.com>
2020-02-23 18:57:24 -0800

    integrate diskManager table creation into Database() class

--- LstoreDB/lstore/db.py
+++ LstoreDB/lstore/db.py
@@ -4,8 +4,8 @@ from lstore.disk_manager import DiskManager
 class Database():
 
     def __init__(self):
-        self.tables = {}  # maps {string name : Table}
-        self.num_tables = 0
+        #self.tables = {}  # maps {string name : Table}
+        #self.num_tables = 0
         self.diskManager = DiskManager()
         pass
 
@@ -24,15 +24,19 @@ class Database():
     :param key: int             #Index of table key in columns
     """
     def create_table(self, name, num_columns, key):
-        table = Table(name, key, num_columns)
-        self.tables[name] = table
-        self.num_tables = self.num_tables + 1
-        return table
+        if (self.diskManager.new_table_file(name, key, num_columns)):  # check if new table file was successfully created
+            table = Table(name, key, num_columns, self.diskManager)
+            return table
+        else:
+            return None
+        #self.tables[name] = table
+        #self.num_tables = self.num_tables + 1
 
     """
     # Deletes the specified table
     """
     def drop_table(self, name):
+        #FIXME
         if name in self.tables:
             del self.tables[name]
             self.num_tables = self.num_tables - 1
@@ -42,4 +46,9 @@ class Database():
     # Retruns table with the passed name
     """
     def get_table(self, name):
-        pass
+        table_metadata = self.diskManager.open_table_file(name)
+        if (len(table_metadata) != 0):  # the table file and metadata exist
+            table = Table(name, table_metadata[PRIMARY_KEY], table_metadata[COLUMNS], self.diskManager)
+            return table
+        else:
+            return None
