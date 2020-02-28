daad20e96b78429bbba0f28d672b3ab56fadfb6d
Kaito Yoshida <58441279+volatio@users.noreply.github.com>
2020-02-27 20:58:36 -0800

    Revert "Mergev2"

--- LstoreDB/m2_tester_part1.py
+++ LstoreDB/m2_tester_part1.py
@@ -11,7 +11,7 @@ query = Query(grades_table)
 
 records = {}
 seed(3562901)
-for i in range(0, 3000):
+for i in range(0, 1000):
     key = 92106429 + i
     records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
     query.insert(*records[key])
@@ -30,7 +30,7 @@ for key in keys:
     #     print('select on', key, ':', record)
 print("Select finished")
 
-for _ in range(3):
+for _ in range(10):
     for key in keys:
         updated_columns = [None, None, None, None, None]
         for i in range(1, grades_table.num_columns):
@@ -46,11 +46,11 @@ for _ in range(3):
                     error = True
             if error:
                 print('update error on', original, 'and', updated_columns, ':', record, ', correct:', records[key])
-            #else:
-            #    print('update on', original, 'and', updated_columns, ':', record)
+            # else:
+            #     print('update on', original, 'and', updated_columns, ':', record)
             updated_columns[i] = None
 print("Update finished")
-
+"""
 for i in range(0, 100):
     r = sorted(sample(range(0, len(keys)), 2))
     column_sum = sum(map(lambda key: records[key][0], keys[r[0]: r[1] + 1]))
@@ -60,5 +60,5 @@ for i in range(0, 100):
     # else:
     #     print('sum on [', keys[r[0]], ',', keys[r[1]], ']: ', column_sum)
 print("Aggregate finished")
-
+"""
 db.close()
