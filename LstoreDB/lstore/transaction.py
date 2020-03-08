23a38101ca075f499171d7325f39e6206d98f20f
Amber Lee <amblee2009@gmail.com>
2020-03-08 15:53:39 -0700

    modify transaction class, fix import statements

--- LstoreDB/lstore/transaction.py
+++ LstoreDB/lstore/transaction.py
@@ -1,5 +1,6 @@
-from template.table import Table, Record
-from template.index import Index
+from lstore.table import Table, Record
+from lstore.index import Index
+from lstore.config import *
 
 class Transaction:
 
@@ -23,17 +24,20 @@ class Transaction:
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
         for query, args in self.queries:
-            result = query(*args)
-            # If the query has failed the transaction should abort
+            result = query(*args, ACQUIRE_LOCK)
+            # If the query has failed to take the locks the transaction should abort
             if result == False:
                 return self.abort()
         return self.commit()
 
     def abort(self):
+        #ask database/lock manager to release the locks taken so far
         #TODO: do roll-back and any other necessary operations
         return False
 
     def commit(self):
+        #actually call the query function to do whatever
+        #then release all locks
         # TODO: commit to database
         return True
 
