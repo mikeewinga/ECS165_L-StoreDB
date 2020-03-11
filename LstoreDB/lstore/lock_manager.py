f55088be529c70e9e777515f045ee6d550a61531
mikeewinga <59758904+mikeewinga@users.noreply.github.com>
2020-03-10 17:06:43 -0700

    Update lock_manager.py

--- LstoreDB/lstore/lock_manager.py
+++ LstoreDB/lstore/lock_manager.py
@@ -32,11 +32,13 @@ class Lock:
         self.row = row
 
 class lockNode:
+
     def __init__(self, parent, nodeID):
         self.nodeID = nodeID
         self.parent = parent
         self.child = {}
         self.locks = [0,0,0,0,0]
+        # locks[0] will never be accessed
     
     def add_lock(self, lock):
         self.locks[lock] += 1
@@ -54,7 +56,7 @@ class lockNode:
             4| n n n n
         """
         # if CurrentLock has S and NewLock wants S
-        if(self.locks[S] > 0 and lock == S):
+        if(self.locks[S] >= 0 and lock == S):
             return 1
         # if the sum of the CurrentLock and NewLock
         # is less than or equal to 4
@@ -76,76 +78,107 @@ class lockTree:
     """
     
     def __init__(self):
-        self.root = lockNode(None, "root")
+        self.root = lockNode(None, 'root')
 
+    # this continues the recursive process to find or create the 
+    # correct lockNode
     def traverse(self, node, path, opperation, lock_change):
-        if(len(path) == 0):
-            if(opperation == "add"):
+        if not len(path):
+            if(opperation == 'add'):
                 if(node.compare_locks(lock_change)):
                     node.add_lock(lock_change)
                     return 1
                 else:
                     return 0
-            if(opperation == "remove"):
+            if(opperation == 'remove'):
                 node.remove_lock(lock_change)
+                return 1
         try:
             node.child[path[0]]
-        except:
+        except (IndexError, KeyError):
             new_node = lockNode(node, path.pop(0))
             node.child[new_node.nodeID] = new_node
-            traverse(new_node, path, opperation, lock_change)
+            if(self.traverse(new_node, path, opperation, lock_change)):
+                return 1
+            return 0
         else:
-            traverse(node.child[path.pop(0)], path, opperation, lock_change)
+            if(self.traverse(node.child[path.pop(0)], path, opperation, lock_change)):
+                return 1
+            return 0
 
+    # this will start the add or remove lock process
     def change_lock(self, path, opperation, lock_change):
+        #
         try:
             self.root.child[path[0]]
-        except:
+        except (IndexError, KeyError):
             new_node = lockNode(self.root, path.pop(0))
             self.root.child[new_node.nodeID] = new_node
-            traverse(new_node, path, opperation, lock_change)
+            if(self.traverse(new_node, path, opperation, lock_change)):
+                return 1
+            return 0
         else:
-            traverse(self.root.child[path.pop(0)], path, opperation, lock_change)
+            if(self.traverse(self.root.child[path.pop(0)], path, opperation, lock_change)): 
+                return 1
+            return 0
 
+    # This will print the nodes of the tree in 
+    # depth-first-search fashion.
     def debug_traverse(self, node):
         print("nodeID: ", node.nodeID)
-        print("parent: ", node.parent)
+        print("parent: ", node.parent.nodeID)
         print("locks: ", node.locks)
+        print("---------")
         for key in node.child:
             try:
                 node.child[key]
-            except:
-                print("END OF TREE")
+            except (IndexError, KeyError):
                 return
             else:
-                debug_traverse(node.child[key])
-        print("END OF TREE")
+                print(node.nodeID)
+                print(u'\u2193')
+                self.debug_traverse(node.child[key])
 
     def debug_print(self):
         print("root")
+        print("---------")
         for key in self.root.child:
             try:
                 self.root.child[key]
-            except:
-                print("END OF TREE")
-                return  
+            except (IndexError, KeyError):
+                pass
             else:
-                debug_traverse(self.root.child[key])
+                print('root')
+                print(u'\u2193')
+                self.debug_traverse(self.root.child[key])
         print("END OF TREE")
+        print("")
 
 
 
 tree = lockTree()
 tree.debug_print()
+print(" ")
 path1 = ["Grades", 1, 58, 275]
 path2 = ["Grades", 0, 68, 175]
 path3 = ["Grades", 0, 68, 177]
-path2 = ["Grades", 1, 55, 275]
+path4 = ["Grades", 1, 55, 275]
 add = "add"
 remove= "remove"
 tree.change_lock(path1, add, 3)
+path1 = ["Grades", 1, 58, 275]
 tree.change_lock(path2, add, 1)
-tree.change_lock(path1, add, 3)
-#tree.change_lock(path1, add, 2)
+path2 = ["Grades", 0, 68, 175]
+tree.change_lock(path3, add, 4)
+path3 = ["Grades", 0, 68, 177]
+tree.change_lock(path3, add, 4)
+path3 = ["Grades", 0, 68, 177]
+tree.change_lock(path1, add, 2)
+path1 = ["Grades", 1, 58, 275]
 tree.debug_print()
 
+tree.change_lock(path1, remove, 2)
+path1 = ["Grades", 1, 58, 275]
+tree.change_lock(path2, remove, 1)
+path2 = ["Grades", 0, 68, 175]
+tree.debug_print()
